707fa802c991adffec7efd48d43d55d72e7f8aa1
rashidov-pc <rih0208@mail.ru>
2022-02-03 08:07:04 +0500

    fullFixed2

--- src/main/java/uz/center/appcompany/repository/WorkerRepository.java
+++ src/main/java/uz/center/appcompany/repository/WorkerRepository.java
@@ -5,4 +5,8 @@ import uz.center.appcompany.entity.Address;
 import uz.center.appcompany.entity.Worker;
 
 public interface WorkerRepository extends JpaRepository<Worker, Integer> {
+    boolean existsByNameAndPhoneNumber(String name, String phoneNumber);
+    boolean existsByNameAndPhoneNumberAndIdNot(String name, String phoneNumber, Integer id);
+    boolean existsByPhoneNumberAndIdNot(String phoneNumber, Integer id);
+    boolean existsByPhoneNumberAndDepartmentId(String phoneNumber, Integer department_id);
 }
--- src/main/java/uz/center/appcompany/service/WorkerService.java
+++ src/main/java/uz/center/appcompany/service/WorkerService.java
@@ -2,6 +2,7 @@ package uz.center.appcompany.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uz.center.appcompany.entity.Address;
 import uz.center.appcompany.entity.Company;
 import uz.center.appcompany.entity.Department;
 import uz.center.appcompany.entity.Worker;
@@ -37,55 +38,74 @@ public class WorkerService {
         return optionalWorker.orElse(null);
     }
 
-
     public ApiResponse addWorker(WorkerDto dto) {
-//        boolean existsByName = departmentRepository.existsByNameAndCompanyId(dto.getName(), dto.getCompanyId());
-//        if (existsByName){
-//            return new ApiResponse("Bunday department mavjud", false);
-//        }
-//        Department department = new Department();
-//        department.setName(dto.getName());
-//
-//        Optional<Company> optionalCompany = companyRepository.findById(dto.getCompanyId());
-//        if (!optionalCompany.isPresent()){
-//            return new ApiResponse("Bunday kompaniya mavjud emas", false);
-//        }
-//
-//        department.setCompany(optionalCompany.get());
-//        departmentRepository.save(department);
-//        return new ApiResponse("Department saved", true);
-        return null;
+        boolean existsByName = workerRepository.existsByNameAndPhoneNumber(dto.getName(), dto.getPhoneNumber());
+        if (existsByName) {
+            return new ApiResponse("Ushbu ishchi avval ro`yxatdan o`tgan", false);
+        }
+        Worker worker = new Worker();
+        worker.setName(dto.getName());
+        worker.setPhoneNumber(dto.getPhoneNumber());
+
+        Optional<Department> optionalDepartment = departmentRepository.findById(dto.getDepartmentId());
+        if (!optionalDepartment.isPresent()) {
+            return new ApiResponse("Bunday Department mavjud emas", false);
+        }
+        boolean existsByPhoneNumberAndDepartmentId = workerRepository.existsByPhoneNumberAndDepartmentId(dto.getPhoneNumber(), dto.getDepartmentId());
+        if (existsByPhoneNumberAndDepartmentId) {
+            return new ApiResponse("Ushbu ishchi ushbu departmentda mavjud", false);
+        }
+        Department department = optionalDepartment.get();
+        worker.setDepartment(department);
+
+        Address address = new Address();
+        address.setHomeNumber(dto.getHomeNumber());
+        address.setStreet(dto.getStreet());
+        Address saveAddress = addressRepository.save(address);
+        worker.setAddress(saveAddress);
+        workerRepository.save(worker);
+        return new ApiResponse("Department saved", true);
     }
 
     public ApiResponse editWorker(Integer id, WorkerDto dto) {
-//        boolean existsByName = departmentRepository.existsByNameAndIdNotAndCompanyId(dto.getName(), id, dto.getCompanyId());
-//        if (existsByName){
-//            return new ApiResponse("Bunday department mavjud", false);
-//        }
-//
-//        Optional<Department> optionalDepartment = departmentRepository.findById(id);
-//        if (!optionalDepartment.isPresent()){
-//            return new ApiResponse("Bunday department mavjud emas", false);
-//        }
-//
-//        Department department = optionalDepartment.get();
-//        department.setName(dto.getName());
-//
-//        Optional<Company> optionalCompany = companyRepository.findById(dto.getCompanyId());
-//        if (!optionalCompany.isPresent()){
-//            return new ApiResponse("Bunday kompaniya mavjud emas", false);
-//        }
-//        department.setCompany(optionalCompany.get());
-//        departmentRepository.save(department);
-//        return new ApiResponse("Edit department", true);
-        return null;
+        boolean existsByName = workerRepository.existsByNameAndPhoneNumberAndIdNot(dto.getName(), dto.getPhoneNumber(), id);
+        if (existsByName) {
+            return new ApiResponse("Bunday ishchi mavjud", false);
+        }
+
+        Optional<Worker> optionalWorker = workerRepository.findById(id);
+        if (!optionalWorker.isPresent()) {
+            return new ApiResponse("Bunday ishchi mavjud emas", false);
+        }
+        Worker worker = optionalWorker.get();
+        boolean existsByPhoneNumberAndIdNot = workerRepository.existsByPhoneNumberAndIdNot(dto.getPhoneNumber(), id);
+        if (existsByPhoneNumberAndIdNot) {
+            return new ApiResponse("Bunday telefon raqam ilgari ro`yxatdan o`tgan", false);
+        }
+        worker.setName(dto.getName());
+        worker.setPhoneNumber(dto.getPhoneNumber());
+
+        Optional<Department> optionalDepartment = departmentRepository.findById(dto.getDepartmentId());
+
+        if (!optionalDepartment.isPresent()) {
+            return new ApiResponse("Bunday department mavjud emas", false);
+        }
+
+        worker.setDepartment(optionalDepartment.get());
+        Address address = new Address();
+        address.setStreet(dto.getStreet());
+        address.setHomeNumber(dto.getHomeNumber());
+        Address savedAddress = addressRepository.save(address);
+        worker.setAddress(savedAddress);
+        workerRepository.save(worker);
+        return new ApiResponse("Edit department", true);
     }
 
     public ApiResponse deleteWorker(Integer id) {
         try {
             workerRepository.deleteById(id);
             return new ApiResponse("Deleted", true);
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ApiResponse("Error", false);
         }
     }
